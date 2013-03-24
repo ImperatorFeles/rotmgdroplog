a81840c839bcac1b1a8a6d8dca508cde63311e53
Greg <ImperatorFeles@gmail.com>
2013-03-23 21:40:19 -0400

    List now pulls from listings in list.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var bigassIDList = [2879, 2976, 2977, 2978, 2563, 2567, 2571, 2572, 2579, 2585, 2590, 2591, 2592, 2593, 3073, 3074, 3075, 3076, 3077, 3078, 3079, 3080, 3081, 3082, 3083, 2608, 3088, 3087, 2612, 2613, 3091, 3096, 2611, 3100, 3101, 3102, 3103, 3104, 3106, 2630, 2631, 3112, 3113, 3114, 2636, 3111, 3121, 3122, 2644, 2645, 3123, 2629, 2649, 2650, 2651, 3120, 2656, 2660, 2661, 2666, 2667, 3148, 3149, 3150, 3151, 3152, 3159, 3160, 3161, 3167, 3169, 2692, 2693, 2694, 2695, 2696, 2697, 2698, 2699, 2700, 2701, 2702, 3182, 2704, 3181, 2706, 2703, 3187, 2709, 3189, 3186, 3191, 2710, 3190, 3194, 3184, 3196, 3197, 2719, 3199, 2708, 2722, 3202, 3203, 2705, 3188, 2727, 2728, 2720, 2707, 2721, 3192, 3204, 2734, 2735, 3195, 2691, 3198, 3185, 2741, 2742, 2733, 2744, 2690, 3193, 2749, 2751, 2752, 2753, 2754, 2755, 2756, 2757, 2758, 2759, 2760, 2761, 2762, 2763, 2764, 2765, 2766, 2771, 2774, 1826, 2785, 2793, 2794, 2324, 2325, 2806, 2809, 2812, 2815, 2818, 2821, 2824, 2827, 2847, 2848, 2850, 2851, 2852, 2853, 2854, 2855, 2856, 2857, 2858, 2859, 2860, 2861, 2865, 2866, 2867];
+var bigassIDList = [];
 
 var fade = true;
 
@@ -21,6 +21,8 @@ $(document).ready(function()
 	addItemsToList(abilitiesList, menu.find("#abilities"));
 	addItemsToList(armorsList, menu.find("#armor"));
 	addItemsToList(othersList, menu.find("#other"));
+
+	generateIDList();
 
 	// render all unfound items
 	renderUnfoundItems();
@@ -119,6 +121,7 @@ $(document).ready(function()
 	});
 });
 
+// adds a list of items to a menu listing
 function addItemsToList(items, list)
 {
 	for (name in items)
@@ -127,6 +130,7 @@ function addItemsToList(items, list)
 	}
 }
 
+// renders a list of ids in list
 function renderList(list)
 {
 	// clear list
@@ -144,6 +148,7 @@ function renderList(list)
 	});
 }
 
+// renders an item with the id, id
 function renderItem(id)
 {
 	var name = items[id][0];
@@ -178,6 +183,7 @@ function renderItem(id)
 	}
 }
 
+// toggles the "found" check box
 function toggleCheckBox(box)
 {
 	var checkbox = box.find(".found-box");
@@ -212,6 +218,7 @@ function toggleCheckBox(box)
 	}
 }
 
+//renders all items flagged as unfound
 function renderUnfoundItems()
 {
 	// clear list
@@ -235,6 +242,7 @@ function renderUnfoundItems()
 	});
 }
 
+// renders all items flagged as found
 function renderFoundItems()
 {
 	// clear list
@@ -258,6 +266,7 @@ function renderFoundItems()
 	});
 }
 
+// renders a list of items based on the search string, item
 function renderSearchItem(item)
 {
 	var list = [];
@@ -297,4 +306,25 @@ function renderSearchItem(item)
 		fade = false;
 		toggleCheckBox($(this))
 	});
+}
+
+// creates the id list from the menu listings
+function generateIDList()
+{
+	for (var name in weaponsList)
+	{
+		bigassIDList = bigassIDList.concat(weaponsList[name]);
+	}
+	for (var name in abilitiesList)
+	{
+		bigassIDList = bigassIDList.concat(abilitiesList[name]);
+	}
+	for (var name in armorsList)
+	{
+		bigassIDList = bigassIDList.concat(armorsList[name]);
+	}
+	for (var name in othersList)
+	{
+		bigassIDList = bigassIDList.concat(othersList[name]);
+	}
 }
